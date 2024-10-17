a4cdae6b047ef04650df5b0f5e70c03f63e0dd11
Paulo Castro <paullocasttro@hotmail.com>
2024-10-17 18:47:01 -0300

    Add files via upload

---
+++ LoopFor.py
@@ -0,0 +1,20 @@
+cores = ["Azul1","Azul2","Verde1","Amarelo","Marron","Preto"]
+for cor in cores:
+    if cor == "Azul2": 
+        print(f"Minha Fruta é:  {cor}")
+        
+        
+numeros = [1,8,10,20]  
+for  numero in numeros:
+    if numero > 10:
+        print(f"Numeros: {numero} - É Maior que 10")  
+        
+        
+cores = ["Azul1","Azul2","Verde1","Amarelo","Marron","Preto"]
+
+for c in cores:
+    gosto = input(f"Voce Gosta ou não da cor {c}?")             
+        
+        
+        
+    
